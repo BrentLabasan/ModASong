cb3dd9b686227bb95f147696811ef000b19b6572
BrentLabasan <brentlabasan@gmail.com>
2016-01-20 23:18:25 -0800

    Angular wired up

--- ModASong/Scripts/ModASong.js
+++ ModASong/Scripts/ModASong.js
@@ -1,1 +1,11 @@
-﻿
+﻿var ModASongApp = angular.module('ModASongApp', []);
+ModASongApp.controller('MainController', function ($scope) {
+    $scope.modifier = "blah";
+
+    $scope.changeModifier(modifier) {
+        $scope.modifier = modifier;
+    }
+
+    $scope.listOfModifiers = [3, 4];
+
+});
