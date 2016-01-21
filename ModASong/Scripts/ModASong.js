4a530997f045e32ec8fa1d725a27a4d41390ffc3
BrentLabasan <brentlabasan@gmail.com>
2016-01-21 00:55:34 -0800

    background pic of search div changes on mouseover of modifier

--- ModASong/Scripts/ModASong.js
+++ ModASong/Scripts/ModASong.js
@@ -2,8 +2,9 @@
 ModASongApp.controller('MainController', function ($scope) {
     $scope.modifier = "blah";
 
-    $scope.changeModifier(modifier) {
+    $scope.changeModifier = function(modifier, url) {
         $scope.modifier = modifier;
+        $scope.url = url;
     }
 
     $scope.listOfModifiers = [3, 4];
